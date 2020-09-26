4eb8aba80c667ac2fcaf637fef3a19a2df493a20
NITIN JAIN <official.nitinjain@gmail.com>
2020-09-25 23:04:56 -0400

    sample spring boot project

---
+++ src/main/java/com/ib/csg/cascade/CascadeApplication.java
@@ -0,0 +1,13 @@
+package com.ib.csg.cascade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CascadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CascadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ib/csg/cascade/CascadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib.csg.cascade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CascadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
