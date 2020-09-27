3bc122aad52e9bf1312afebf84626596ac0b8626
NITIN JAIN <official.nitinjain@gmail.com>
2020-09-27 13:42:05 -0400

    passing get values to view

--- src/main/java/com/ib/csg/cascade/controller/LoginController.java
+++ src/main/java/com/ib/csg/cascade/controller/LoginController.java
@@ -2,14 +2,20 @@ package com.ib.csg.cascade.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
 
   @RequestMapping("/login")
-  public String loginMessage(){
+  // Adding @RequestParam to get data from Request.
+  // Adding ModelMap to that controller can put data in model which can be retrieved by view.
+  public String loginMessage(@RequestParam String name, ModelMap model)
+  {
+    model.put("name", name);
     return "login";
   }
 }
